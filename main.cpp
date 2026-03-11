ff1c198970bb4a2a33cdd03e626fc648a875fc56
Kyu Lee <kwooler@gmail.com>
2026-03-11 01:10:11 -0700

    Student starter code

--- main.cpp
+++ main.cpp
@@ -10,15 +10,5 @@ int main()
 	cin >> N;
 
 	// TODO
-	cout << n1 << "\t";
-	cout << n2 << "\t";
-	for (int i = 0; i < N - 2; i++)
-	{
-		sum = n1 + n2;
-		cout << sum << "\t";
-		n1 = n2;
-		n2 = sum;
-	}
-	cout << endl;
 	// TODO
 }
