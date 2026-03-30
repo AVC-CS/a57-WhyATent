e490cc4ed15bd12be731a7504e0ce8bfe1695d99
root <root@MH320-78120.avc.edu>
2026-03-30 12:53:02 -0700

    a57 Done. Added text for clarification when using program.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@ int main()
 	int n1, n2;
 	int N, sum;
 
+	cout << "Input two values separated by spaces.\n";
 	cin >> n1 >> n2;
+	cout << "Enter number of iterations (N>2)\n";
 	cin >> N;
-
-	// TODO
-	// TODO
+	cout << n1 << endl << n2 << endl;
+	for (int i=0;i<N-2;i++){
+		sum = n1 + n2;
+		cout << sum << endl;
+		n1 = n2;
+		n2 = sum;
+	}
 }
